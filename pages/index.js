884efde428f4152baed9b77c23c3d790160fdd1b
Kaique Kira <83593559+kaique-kira@users.noreply.github.com>
2022-01-24 22:43:03 -0300

    CORES
    Adicionando cor ao perfil

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
+function Title() {
+return(
+  <h1>Seja Bem-Vindo Nakama!!!</h1>
+);
+}
+
 function HomePage() {
-    return <div>Welcome to Next.js!</div>
+    return <div>
+
+      <Title>Seja Bem-Vindo Nakama!!!</Title>
+      <h2>Discord Kaique - One Piece</h2>
+      <style jsx>{`
+     
+      h1 {
+        color: red;
+      }
+    `}</style>
+      </div>
   }
   
   export default HomePage
